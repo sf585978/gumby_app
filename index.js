0d116163eaafdc284583ca9deb920fd2fff7b11b
Sean Fischer <sf585978@gmail.com>
2022-01-08 20:57:21 +0000

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ client.on("message", function(message) {
 	
 	if (command === "shitpost") {
 		//message.channel.send(`It looks like someone has been shitposting! Cut it out!`, {files: ["https://i.kym-cdn.com/photos/images/facebook/000/866/490/6c4.png"]});
-		 if (Math.random() < 0.15) {
+		 if (Math.random() < 0.18) {
 			message.channel.send(`Looks like someone's been shitposting! Cut it out!`, {files: ["https://i.kym-cdn.com/photos/images/facebook/000/866/490/6c4.png"]});
 		 } else {
 			message.channel.send(`I looked into this post and determined it is not a shitpost!`, {files: ["https://media.makeameme.org/created/one-does-not-5b5c3e.jpg"]});
